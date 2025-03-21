7ef3dae9d012b24034ad983ada07efae67ff3ac5
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2025-03-21 00:26:19 -0600

    modificaciones para ver base

--- CrudConsumoCFE/settings.py
+++ CrudConsumoCFE/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = config(
 # Application definition
 
 INSTALLED_APPS = [
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
