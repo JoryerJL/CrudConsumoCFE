3e0e16a37f3d9ca67bbb581ad3a1f28a8ec447c4
Joryer Jimenez <146901957+JoryerJL@users.noreply.github.com>
2025-03-21 08:26:31 -0600

    haciendo modelos, client y service

--- client/models.py
+++ client/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from common.models import CommonBaseModel
+
+class State(CommonBaseModel):
+    name = models.CharField("Nombre", max_length=100)
+    def __str__(self):
+        return self.name
+
+class Municipality(CommonBaseModel):
+    name = models.CharField("Nombre", max_length=100)
+    state = models.ForeignKey(State, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.name
+
+class PostalCode(CommonBaseModel):
+    postal_code = models.PositiveBigIntegerField("Código postal")
+    municipality = models.ForeignKey('Municipality', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.postal_code
+
+class Colony(CommonBaseModel):
+    name = models.CharField("Nombre", max_length=100)
+    postal_code = models.CharField("Código postal", max_length=5)
+
+    def __str__(self):
+        return self.name
 
 # Create your models here.
+class Client(CommonBaseModel):
+    name = models.CharField("Nombre", max_length=100)
+    paternal_surname = models.CharField("Apellido Paterno", max_length=100)
+    maternal_surname = models.CharField("Apellido Materno", max_length=100)
+    phone_number = models.CharField("Número de teléfono", max_length=10)
+    street = models.CharField("Calle", max_length=100)
+    interior_number = models.PositiveIntegerField("Número interior", null=True, blank=True)
+    external_number = models.PositiveIntegerField("Exteranl interior", null=True, blank=True)
+    colony = models.ForeignKey(Colony, on_delete=models.CASCADE)
+    postal_code = models.CharField("Código postal", max_length=5)
+
--- service/models.py
+++ service/models.py
@@ -1,1 +1,28 @@
 from django.db import models
+from client.models import Client
+from common.models import CommonBaseModel
+
+class Rate(CommonBaseModel):
+    rate = models.DecimalField(max_digits=10, decimal_places=2)
+
+class Service(CommonBaseModel):
+    service_number = models.PositiveIntegerField("Número de servicio")
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    meter_number = models.CharField("Número de medidor",max_length=11)
+    rmu = models.CharField("RMU",max_length=100)
+    rate = models.ForeignKey('Rate', on_delete=models.CASCADE)
+    threads = models.PositiveIntegerField("Hilos")
+    account = models.CharField("Cuenta",max_length=100)
+
+
+class Receipt(CommonBaseModel):
+    service = models.ForeignKey('Service', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    previous_kWh_consumption = models.DecimalField("Consumo de kW anterior", max_digits=10, decimal_places=2)
+    current_kWh_consumption = models.DecimalField("Consumo de kWh actual", max_digits=10, decimal_places=2)
+    start_period = models.DateField("Fecha de inicio del periodo facturado")
+    end_period = models.DateField("Fecha de fin del periodo facturado")
+    total_kWh_consumption = models.DecimalField("Consumo total de kWh", max_digits=10, decimal_places=2)
+    payment_deadline = models.DateField("Límite de pago")
+    cut_off_date = models.DateField("Fecha de corte")
+
